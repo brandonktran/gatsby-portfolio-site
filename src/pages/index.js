8ec92a9c190924e13d444dd2281b5ecfe79dcfd2
brandonktran <brandontran@berkeley.edu>
2021-09-17 23:59:30 -0700

    update helmet

--- src/pages/index.js
+++ src/pages/index.js
@@ -123,23 +123,6 @@ const IndexPage = () => {
         <link href="css/styles.css" rel="stylesheet" />
         {/* <!-- AOS --> */}
         {/* <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" /> */}
-
-        <script type="text/javascript">
-          bg = new Image() bg.src = "./assets/img/bg.jpg"
-        </script>
-        {/* <!-- Bootstrap core JS--> */}
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
-        {/* <!-- Third party plugin JS--> */}
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
-        {/* <!-- Contact form JS--> */}
-        <script src="assets/mail/jqBootstrapValidation.js"></script>
-        <script src="assets/mail/contact_me.js"></script>
-        {/* <!-- Core theme JS--> */}
-        {/* <script src="../scripts.js"></script> */}
-        {/* <!-- AOS --> */}
-        <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-        <script>AOS.init();</script>
       </Helmet>
       <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
         <div class="container">
